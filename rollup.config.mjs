29e21f9214c9e52cf07f5e792f48925bb0492c9a
admon84 <admonwest84@gmail.com>
2023-08-14 19:05:51 -0600

    migrate rollup-plugin-screeps to rollup-plugin-screeps-world

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ import clear from 'rollup-plugin-clear';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import screeps from 'rollup-plugin-screeps';
+import screeps from 'rollup-plugin-screeps-world';
 
 let cfg;
 const dest = process.env.DEST;
